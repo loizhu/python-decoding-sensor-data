b1e287f94559176b0f18eb9ea13bf446eb6c32e8
loizhu <62029180+loizhu@users.noreply.github.com>
2023-04-24 10:39:48 -0400

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -69,3 +69,9 @@ concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
